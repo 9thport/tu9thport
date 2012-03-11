47f8c977d14ba0d8b8002cf000fdb71641431be3
aaron <aaron@9thport.net>
2012-03-11 13:13:33 -0700

    added files for rails 3.2.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tu9thportNet::Application.config.secret_token = '5fa8b49703a90ab1b3aa5f01cf5335e4dd4828427a359d3c3d08017668c16e4bc2fa2e9ec076d738f566dff6c7080290b5a66bfbd922abc94353a690f0dc66d6'
