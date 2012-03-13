d381f0efe0aef75028ea4a38dd2e30eccf9e520d
aaron <aaron@9thport.net>
2012-03-12 20:07:45 -0700

    added custom rack env for production

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
+if ENV["RACK_ENV"] == "production"
+  ENV['GEM_PATH'] = File.expand_path('~/.rvm/gems/ruby-1.8.7-p72@tu.9thport.net') + ':/usr/lib/ruby/gems/1.8'
+end
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
